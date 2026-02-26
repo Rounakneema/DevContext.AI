7d3f6a26b13b27040e68ea5a444b482049373e1a
Laxita Jain <laxitajain912@gmail.com>
2026-02-26 20:41:56 +0530

    protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Sidebar from "./components/Sidebar";
 import HomePage from "./pages/HomePage";
 import LoadingPage from "./pages/LoadingPage";
@@ -18,20 +19,22 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
 
-          {/* Main App Routes with Sidebar */}
+          {/* Main App Routes with Sidebar - Protected */}
           <Route
             path="/*"
             element={
-              <div className="app">
-                <Sidebar />
-                <div className="main-content">
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/loading" element={<LoadingPage />} />
-                    <Route path="/dashboard" element={<DashboardPage />} />
-                  </Routes>
+              <ProtectedRoute>
+                <div className="app">
+                  <Sidebar />
+                  <div className="main-content">
+                    <Routes>
+                      <Route path="/" element={<HomePage />} />
+                      <Route path="/loading" element={<LoadingPage />} />
+                      <Route path="/dashboard" element={<DashboardPage />} />
+                    </Routes>
+                  </div>
                 </div>
-              </div>
+              </ProtectedRoute>
             }
           />
         </Routes>
